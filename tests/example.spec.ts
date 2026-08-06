adecce27c55e4222f737176f852345121f67ae9f
Natalia Firyn <natalia.firyn@gmail.com>
2026-08-06 19:23:02 +0200

    chore(playwright): update configuration and simplify test URL

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('has Rolnopol title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   await expect(page).toHaveTitle(/Rolnopol/);
 });
