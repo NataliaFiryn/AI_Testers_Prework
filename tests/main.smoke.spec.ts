35a7d35c260acb544ad7f0aeb691fb8e13755b91
Natalia Firyn <natalia.firyn@gmail.com>
2026-08-13 18:42:22 +0200

    test(smoke): expand page coverage

--- tests/main.smoke.spec.ts
+++ tests/main.smoke.spec.ts
@@ -1,9 +1,45 @@
 import { expect, test } from '@playwright/test';
 
-for (const { name, path, title } of [
-  { name: 'home', path: '/', title: 'Rolnopol' },
-  { name: 'login', path: '/login.html', title: 'Login - Rolnopol' },
-  { name: 'register', path: '/register.html', title: 'Register - Rolnopol' }
+for (const {
+  name,
+  path,
+  title,
+  headerSelector,
+  headerText,
+  additionalText
+} of [
+  {
+    name: 'home',
+    path: '/',
+    title: 'Rolnopol',
+    headerSelector: '.main-title',
+    headerText: 'Rolnopol',
+    additionalText: 'Futuristic Farm & Resource Management'
+  },
+  {
+    name: 'alerts',
+    path: '/alerts.html',
+    title: 'Alerts - Rolnopol',
+    headerSelector: '.main-title',
+    headerText: 'Rolnopol',
+    additionalText: 'Alerts and Notifications'
+  },
+  {
+    name: 'documentation',
+    path: '/docs.html',
+    title: 'Documentation - Rolnopol',
+    headerSelector: '.main-title',
+    headerText: 'Documentation',
+    additionalText: 'Rolnopol System Guide & API Reference'
+  },
+  {
+    name: 'register',
+    path: '/register.html',
+    title: 'Register - Rolnopol',
+    headerSelector: '.main-title',
+    headerText: 'Rolnopol',
+    additionalText: 'Create Your User Account'
+  }
 ]) {
   test(
     `should load and display the ${name} page`,
@@ -11,9 +47,32 @@ for (const { name, path, title } of [
     async ({ page }) => {
       const response = await page.goto(path);
 
+      expect(response).not.toBeNull();
       expect(response?.ok()).toBe(true);
       await expect(page.locator('body')).toBeVisible();
       await expect(page).toHaveTitle(title);
+      await expect(page.locator(headerSelector)).toBeVisible();
+      await expect(page.locator(headerSelector)).toHaveText(headerText);
+      await expect(
+        page.locator('header').getByText(additionalText, { exact: true })
+      ).toBeVisible();
     }
   );
 }
+
+test(
+  'should load and display the Swagger page',
+  { tag: '@smoke' },
+  async ({ page }) => {
+    const response = await page.goto('/swagger.html');
+
+    expect(response).not.toBeNull();
+    expect(response?.ok()).toBe(true);
+    await expect(page.locator('body')).toBeVisible();
+    await expect(page).toHaveTitle('Rolnopol - Swagger');
+    const swaggerTitle = page.frameLocator('iframe').locator('.title');
+
+    await expect(swaggerTitle).toBeVisible();
+    await expect(swaggerTitle).toContainText('Rolnopol');
+  }
+);
